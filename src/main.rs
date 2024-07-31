5d9da9a9b1d5e1705762a7758870b824d80da5fb
Jan Lafferton <jan@lafferton.de>
2024-07-31 21:35:22 +0200

    stuff

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,17 @@ use bitmatch::bitmatch;
 use std::{collections::HashMap, sync::LazyLock};
 
 struct Context {
-    regs: [u16; 7],
+    regs: [u8; 16],
+}
+
+impl Context {
+    fn read_reg8(&self, reg: u8) -> u8 {
+        self.regs[reg as usize]
+    }
+
+    fn write_reg8(&mut self, reg: u8, value: u8) {
+        self.regs[reg as usize] = value;
+    }
 }
 
 fn nop(_: &mut Context) -> TStates {
@@ -12,9 +22,18 @@ fn nop(_: &mut Context) -> TStates {
 type TStates = (u8, u8);
 type OpcodeImpl = Box<dyn Fn(&mut Context) -> TStates>;
 
-fn implement_ld(dest: usize, source: usize) -> OpcodeImpl {
-    Box::new(move |ctx: &mut Context| {
-        ctx.regs[dest] = ctx.regs[source];
+fn ld8(dst: u8, src: u8) -> OpcodeImpl {
+    Box::new(move |ctx| {
+        let val = ctx.read_reg8(src);
+        ctx.write_reg8(dst, val);
+        (1, 4)
+    })
+}
+
+fn ld16(dst: u16, src: u16) -> OpcodeImpl {
+    Box::new(move |ctx| {
+        let val = ctx.read_reg8(src);
+        ctx.write_reg8(dst, val);
         (1, 4)
     })
 }
@@ -23,7 +42,8 @@ fn implement_ld(dest: usize, source: usize) -> OpcodeImpl {
 fn implement_opcode(opcode: u8) -> OpcodeImpl {
     #[bitmatch]
     match opcode {
-        "01dd_dsss" => implement_ld(d as usize, s as usize),
+        "01dd_dsss" => ld8(d.into(), s.into()),
+        "00dd_d110" => ld_const(d.into()),
         _ => Box::new(nop),
     }
 }
@@ -33,5 +53,5 @@ fn main() {
         .map(|opcode| (opcode, implement_opcode(opcode)))
         .collect::<HashMap<u8, OpcodeImpl>>();
 
-    opcode_table[&0x00](&mut Context { regs: [0; 7] });
+    opcode_table[&0x00](&mut Context { regs: [0; 16] });
 }
